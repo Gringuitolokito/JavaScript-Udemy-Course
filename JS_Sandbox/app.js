ec8f02cbf025668ed0405a429a4cbabb0c24ff12
remote-upload <remoteupload@git.edu>
2019-01-05 14:50:13 -0700

    ..

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,99 @@
+// Log to console
+// var, let, const are all ways to declare variables
+
+//var name = 'John Doe';
+//
+//console.log(name);
+//
+//name =  'Steve Jobs';
+//
+//console.log(name);
+
+// Init var
+
+//var greeting; 
+//
+//console.log(greeting);
+//
+//greeting = 'Hello';
+//
+//console.log(greeting);
+
+// letters, numbers, _, $
+// Cannot start with a number
+
+// Multi-word vars
+//var firstName = 'John'; // Camel case
+//var first_name = 'Sierra'; // Underscore
+//var FristName = 'Tom'; //Pascol case
+
+// Using LET
+
+//let name = 'John Doe';
+//
+// Using CONST
+//
+//const name = "john";
+
+// CONST cannot be reassigned
+// Have to assign a value
+
+//const person = {
+//    name: "John",
+//    age: 30
+//}
+
+// You can change the name and age with the following code
+
+//person.name = "Sara";
+//person.age = 32;
+
+//console.log(person);
+
+//const numbers = [1,2,3,4,5];
+
+// You can add to this array by using the following code
+
+//numbers.push(6);
+
+//console.log(numbers);
+
+// You can change const values when inside of an array or object
+
+
+
+//let val;
+
+// Number to string
+
+//val = String(5);
+
+//val = String(4+4);
+
+// Convert boolean
+//val = String(true);
+
+// Date to a string
+//val = String(new Date());
+
+// Array to string
+
+//val = String([1,2,3,4,5]);
+
+// Another method to convert a variable to a string would be to use //toString()
+
+//val = (5).toString();
+
+// String to number
+//val = Number('5');
+
+// Output
+
+//console.log(val);
+//console.log(typeof val);
+
+// This console only works for strings
+//onsole.log(val.length);
+
+// this one works only with numbers
+//console.log(val.toFixed(2));
